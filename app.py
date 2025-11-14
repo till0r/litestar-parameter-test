305ebbf92df3eb2b2db853685be172683a61839e
till0r <till0r@users.noreply.github.com>
2025-11-14 11:15:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import datetime
+from dataclasses import dataclass
+from typing import Annotated
 
 from litestar import Litestar, get
+from litestar.params import Parameter
+
+
+@dataclass
+class ParameterSet:
+    some_date: datetime.date | None = None
+    some_bool: bool | None = None
+
+
+DTorNone = datetime.date | None
 
 
 @get("/params")
-async def params(
-    some_date: datetime.date | None = None, some_bool: bool | None = None
-) -> str:
-    if some_date:
-        return "Great date!"
-    if some_bool:
-        return "Great bool!"
+async def params(query: dict) -> str:
+    print(query)
+    if query.get("some_date"):
+        some_date = query.get("some_date")
+        return f"Great date! {some_date}!"
     return "No params?"
 
 
